4c98e430f8c856d483dbd15a6b8a7edf3fea9a93
Dinu-Filip <dinu.filip.self@gmail.com>
2024-02-05 16:02:39 +0000

    Completed round summary window

--- integral_bee_app/lib/match.dart
+++ integral_bee_app/lib/match.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'dart:io';
 import 'dart:math';
 
@@ -28,6 +29,9 @@ class MatchState extends State<Match> {
   final List<String> rounds = ["Quarterfinal", "Semifinal", "Final"];
   static const String playerFile = "player.txt";
   static const List<String> schools = ["Beths Grammar School"];
+  static const Map<String, String> schoolCode = {
+    "Beths Grammar School": "Beths"
+  };
   bool initialised = false;
   final Map<String, int> schoolPoints = {};
   List<Player> participants = [];
@@ -107,6 +111,29 @@ class MatchState extends State<Match> {
       }
     }
     matches[0] = initialPairings;
+    setState(() {
+      initialised = true;
+    });
+  }
+
+  int numberOfIntegrals(String round) {
+    if (round == "Semifinal" || round == "Final") {
+      return 5;
+    } else {
+      return 3;
+    }
+  }
+
+  double timePerIntegral(String round) {
+    if (round == "Quarterfinal") {
+      return 3;
+    } else if (round == "Semifinal") {
+      return 4;
+    } else if (round == "Final") {
+      return 5;
+    } else {
+      return 2.5;
+    }
   }
 
   @override
@@ -116,7 +143,79 @@ class MatchState extends State<Match> {
         setRounds();
         initialiseMatches();
       });
+      return LoadingAnimationWidget.stretchedDots(
+          color: Colors.black, size: 50);
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(rounds[currentRound],
+                  style: const TextStyle(
+                      fontSize: 60, fontWeight: FontWeight.bold))),
+          Text("Number of participants: ${matches[currentRound].length * 2}",
+              style: const TextStyle(fontSize: 40)),
+          const Padding(
+              padding: EdgeInsets.all(15),
+              child: FractionallySizedBox(
+                  widthFactor: 0.6, child: Divider(color: Colors.black))),
+          Text("${numberOfIntegrals(rounds[currentRound])} integrals",
+              style: const TextStyle(fontSize: 40)),
+          Text("${timePerIntegral(rounds[currentRound])} minutes per integral",
+              style: const TextStyle(fontSize: 40)),
+          const Padding(
+              padding: EdgeInsets.all(15),
+              child: FractionallySizedBox(
+                  widthFactor: 0.6, child: Divider(color: Colors.black))),
+          const Text("Draw",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w600)),
+          const SizedBox(height: 15),
+          FractionallySizedBox(
+              widthFactor: 0.6,
+              child: SingleChildScrollView(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: matches[currentRound]
+                            .map((match) => () {
+                                  return Text(
+                                      "${match[0].name} (${schoolCode[match[0].school]})",
+                                      style: const TextStyle(fontSize: 30));
+                                }())
+                            .toList()),
+                    (() {
+                      List<Text> items = [];
+                      for (int i = 0; i < matches[currentRound].length; i++) {
+                        items.add(
+                            const Text("vs.", style: TextStyle(fontSize: 30)));
+                      }
+                      return Column(children: items);
+                    }()),
+                    Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: matches[currentRound]
+                            .map((match) => () {
+                                  return Text(
+                                      "${match[1].name} (${schoolCode[match[1].school]})",
+                                      style: const TextStyle(fontSize: 30));
+                                }())
+                            .toList())
+                  ]))),
+          Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: FractionallySizedBox(
+                  widthFactor: 0.3,
+                  child: SizedBox(
+                      height: 60,
+                      child: OutlinedButton(
+                          onPressed: () {}(),
+                          child: const Text("Start round",
+                              style: TextStyle(fontSize: 25))))))
+        ],
+      );
     }
-    return const Text("");
   }
 }
